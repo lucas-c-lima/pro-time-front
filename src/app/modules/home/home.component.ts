3e4f3a669edc88a59cce2e3e1e98bac189aff154
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-03-02 19:13:21 -0300

    add HomeComponent with login form and styles; configure routing

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm(): void {
+    console.log('Dados do formulário de login', this.loginForm.value)
+  }
+}
