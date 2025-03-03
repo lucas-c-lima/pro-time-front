c3d626423f4fe122a8d2768f7b87a78bb2d9230e
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-03-03 13:41:16 -0300

    add dashboard module with routing and auth guard; implement dashboard home component; update user service for token handling

---
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-home',
+  templateUrl: './dashboard-home.component.html',
+  // TODO apagar caso não precise mudar estilos (quase impossivel mas né)
+  styleUrls: ['./dashboard-home.component.scss']
+})
+export class DashboardHomeComponent {
+
+}
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -5,6 +5,7 @@ import { jwtDecode } from 'jwt-decode';
 import { AuthRequest } from 'src/app/models/interface/user/auth/AuthRequest';
 import { AuthResponse } from 'src/app/models/interface/user/auth/AuthResponse';
 import { environment } from 'src/environments/environments';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ import { environment } from 'src/environments/environments';
 export class UserService {
   private API_URL = environment.API_URL;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookie: CookieService) { }
 
   authUser(requestDatas: AuthRequest): Observable<AuthResponse>{
     return this.http.post<AuthResponse>(`${this.API_URL}/auth/login`, requestDatas)
@@ -26,5 +27,10 @@ export class UserService {
   getProfileFromToken(token: string): string {
     const decodedToken: any = jwtDecode(token);
     return decodedToken.userProfile || null;
+  }
+
+  isLoggedIn(): boolean{
+    const JWT_TOKEN = this.cookie.get("USER_TOKEN")
+    return JWT_TOKEN ? true : false;
   }
 }
