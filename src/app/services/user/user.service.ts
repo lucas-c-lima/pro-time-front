43123f8beff7786f4d03b8fe7e6c2997a1e137ee
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-03-02 22:52:35 -0300

    add AuthRequest and AuthResponse interfaces; implement UserService for authentication

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthRequest } from 'src/app/models/interface/user/auth/AuthRequest';
+import { AuthResponse } from 'src/app/models/interface/user/auth/AuthResponse';
+import { environment } from 'src/environments/environments';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  authUser(requestDatas: AuthRequest): Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(`${this.API_URL}/auth/login`, requestDatas)
+  }
+}
