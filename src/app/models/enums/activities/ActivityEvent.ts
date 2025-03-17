b56f5e11d81724acfcfdcd0f3b8a02d336cc40d1
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-03-16 21:04:25 -0300

    feat(activities) update activity event labels and enhance form styling for better user experience

--- src/app/models/enums/activities/ActivityEvent.ts
+++ src/app/models/enums/activities/ActivityEvent.ts
@@ -1,5 +1,5 @@
 export enum ActivityEvent {
   ADD_ACTIVITY_EVENT = 'Adicionar Atividade',
-  EDIT_ACTIVITY_EVENT = 'Editar Atividade',
+  EDIT_ACTIVITY_EVENT = 'Detalhes da Atividade',
   HOURS_ACTIVITY_EVENT = 'Registrar Horas'
 }
